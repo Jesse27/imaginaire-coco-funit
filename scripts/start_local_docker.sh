966eb532e99b40cfc548fabfd3416b8ea569b8a7
Jesse27 <30261678+Jesse27@users.noreply.github.com>
2022-11-06 22:35:03 +1100

    adds name and tensorboard port to container

--- scripts/start_local_docker.sh
+++ scripts/start_local_docker.sh
@@ -2,9 +2,11 @@ docker run \
     --gpus all \
     --shm-size 32g \
     --ipc=host \
+    --name coco-funit
     -it \
     -v /mnt:/mnt \
     -v ~/:/home \
+    -p 8083:6006 \
     nvcr.io/nvidian/lpr-imagine/imaginaire:${1}-py3 \
     /bin/bash
 
